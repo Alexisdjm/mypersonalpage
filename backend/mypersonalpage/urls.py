8ad44b479a04194c212afbf3872e508ec83210b2
Alexis Jimenez <Alexis.jm29@gmail.com>
2023-08-27 22:50:00 -0400

    static

--- backend/mypersonalpage/urls.py
+++ backend/mypersonalpage/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.templatetags.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,3 +26,7 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     path('about', TemplateView.as_view(template_name='index.html'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
