2acfd20fffb282393b53c086f8b150656b0ab618
Alexis Jimenez <Alexis.jm29@gmail.com>
2023-09-03 17:59:02 -0400

    static

--- backend/mypersonalpage/urls.py
+++ backend/mypersonalpage/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path('api/', include('core.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
     path('about', TemplateView.as_view(template_name='index.html'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
